4a0cf98a0e81b3e3460d05829cb87f4245b47c33
Jenilla Sweetlyn <jenillasweetlyn@gmail.com>
2026-07-26 20:58:40 +0530

    Configured React Router

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Products from "../pages/Products";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
